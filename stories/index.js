f4e64cc85331efb62141fa766339f899c393f220
Lior Belinsky <liorbe@wix.com>
2017-11-20 22:39:02 +0200

    add Button component

--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import Hello from '../src/Hello';
+import Button from '../src/components/Button';
 
-storiesOf('Hello', module)
-  .add('default view', () => (
-    <Hello name="world"/>
-  ))
-  .add('with emojis', () => (
-    <Hello name="😀 😎 👍"/>
+storiesOf('Button', module)
+  .add('Button', () => (
+    <Button>Hello</Button>
   ));
