b64e10c3e60cb5c0be16aba51f1716dcbecab473
Lior Belinsky <liorbe@wix.com>
2017-11-20 22:37:45 +0200

    add backoffice initial pallete

---
+++ src/pallete.js
@@ -0,0 +1,11 @@
+export const pallete = {
+  main: '#3899ec', //B10
+  mainHover: '#4eb7f5', //B20
+  notifications: '#c1e4fe', //B30
+  dividers: '#eaf7ff', //B50
+  danger: '#ee5951', //R10
+  dangerHover: '#ff6666', //R20
+  mainInputText: '#162d3d', //D10
+  disabled: '#cbd3dc', //D55
+  white: '#ffffff' //D80
+};
